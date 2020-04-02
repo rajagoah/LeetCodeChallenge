f90563c9df3f942da06bfc83dce469f8cbba9989
Aakarsh Rajagopalan <51793634+rajagoah@users.noreply.github.com>
2020-04-02 17:27:21 -0400

    challenge 2

--- Week-1 challenge 1 -Happy numbers.py
+++ Week-1 challenge 1 -Happy numbers.py
@@ -5,23 +5,31 @@
 #and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in
 #a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers
 
-num = 11
+num = 345
 
-#coverting to string to iterate through it
+# coverting to string to iterate through it
 num_Str = str(num)
 
-#declaring variables
+# declaring variables
 happy_num = 0
-lst_num = []
 
-#iterating through the string
-while len(str(num_Str)) != 1:
+#Using while loop to make the loop continue performing calculations till the Happy number result is reached
+while happy_num != 1:
+
+    #declaring variables
+    happy_num = 0
+    lst_num = []
+
+    #iterating through the string
     for i in range(len(num_Str)):
         lst_num.append(int(num_Str[i]))
 
     for i in lst_num:
         happy_num = (i*i) + happy_num
-        print(happy_num)
-        num_Str = str(happy_num)
+    num_Str = str(happy_num)
 
+if happy_num == 1:
+    print('True')
+else:
+    print('False')
 
