07e9c4f0c31ad0dc6e350cb90fd44251664b8277
Aakarsh Rajagopalan <51793634+rajagoah@users.noreply.github.com>
2020-04-09 09:29:33 -0400

    ReverseInt updated code

--- ReverseInt Solution 2.py
+++ ReverseInt Solution 2.py
@@ -1,14 +1,24 @@
 def ReverseInt(x):
     #declaring a temporary holding variable (temp) and a variable to hold the reverse of the number (rev)
+    current = abs(x)
     temp = 0
     rev = 0
-    #iterating through the digits in the varaible
-    while x != 0:
-        #pop operation (achieved by using mod operator and push operation achieved by the summation operation)
-        temp = temp*10 + x%10
-        x = x//10
-        rev = temp
-    return rev
+
+    if current >= 0:
+        #iterating through the digits in the varaible
+        while current != 0:
+            #pop operation (achieved by using mod operator and push operation achieved by the summation operation)
+            temp = temp*10 + current%10
+            current = current//10
+            rev = temp
+        return rev
+    if current < 0:
+       while current != 0:
+           # pop operation (achieved by using mod operator and push operation achieved by the summation operation)
+           temp = temp * 10 + current % 10
+           current = current // 10
+           rev = temp
+       return rev*-1
 
 if __name__=="__main__":
-    print(ReverseInt(123))
+    print(ReverseInt(-656))
