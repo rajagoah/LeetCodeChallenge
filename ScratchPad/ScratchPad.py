ec01ec736c8079d9857de35a90dcd89488b46fab
Aakarsh Rajagopalan <51793634+rajagoah@users.noreply.github.com>
2020-04-14 17:07:44 -0400

    Scratch pad
    Code for practicing the class creation. Understood today how the address value gets transferred across variables during the creation of the nodes() variable

--- ScratchPad/ScratchPad.py
+++ ScratchPad/ScratchPad.py
@@ -1,8 +1,40 @@
 class Computer():
-    pass
+    def __init__(self):
+        self.age = 21
+        self.name = "Raj"
 
+class nodes():
+    def __init__(self, data):
+        self.d = data
+        self.next = None
+
+class linked():
+    def __init__(self):
+        self.head = None
+
+#calling computer class
 c1 = Computer()
-c2 = Computer()
 
 print("id ", id(c1))
-print("id ", id(c2))
+print("the memory address is: ", c1)
+
+#calling node class
+n = nodes(1)
+print("the details of the node are: ", n)
+
+#calling linked class
+l = linked()
+print("the details of the linked object are: ", l)
+print("calling the field in the linked class: ", l.head)
+
+#creating a main function where all the fields can be called within any class
+if __name__=="__main__":
+    combined_obj = linked()
+    print("the combined object has been created to be of Linked() class ", type(combined_obj))
+
+    x = combined_obj.head = nodes(1)
+    print("the combined object's head field has been instantiated with the nodes class ", type(x))
+    print("the memory of the combined_obj.head variable of nodes() class is, ", x)
+
+
+
